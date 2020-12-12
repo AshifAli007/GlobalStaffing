9de8e576a661f8299be2a1ff5eaf9c23feb3cedb
Ashif <theashifkhan@gmail.com>
2020-12-12 20:12:44 +0530

    video added

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+
+// -------------------------video javascript starts here-------------------------------------------
+
+(function ($, window, document, undefined) {
+
+    'use strict';
+
+
+    $(function () {
+        app.init();
+    });
+
+})($, window, document);
+
+var app = {
+  init: function(){
+    console.log('Ready');
+    
+    var player = new Plyr($('#player'));
+    
+    $('.bd-example-modal-lg').on('hide.bs.modal', function (e) {
+        player.stop();
+
+      })
+  }
+}
+
+// -------------------------video javascript ends here-------------------------------------
